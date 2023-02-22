e7fd850a02cbb9bc49b8639e346f187b3f3bb58c
Mohamedtwfiek-7 <mohamedtwfiek7@gmail.com>
2023-02-22 10:43:04 +0200

    fixing all errors and make the AI smarter and handle the orientation of the device to make it some responsive

--- lib/game_logic.dart
+++ lib/game_logic.dart
@@ -27,21 +27,23 @@ class Game {
 
   checkWinner() {
     String winner = '';
+
     if (Player.playerX.containsAll(0, 1, 2) ||
         Player.playerX.containsAll(3, 4, 5) ||
         Player.playerX.containsAll(6, 7, 8) ||
         Player.playerX.containsAll(0, 3, 6) ||
         Player.playerX.containsAll(1, 4, 7) ||
         Player.playerX.containsAll(2, 5, 8) ||
-        Player.playerX.containsAll(1, 4, 8) ||
-        Player.playerX.containsAll(2, 4, 6)) winner = 'X';
-    if (Player.playerO.containsAll(0, 1, 2) ||
+        Player.playerX.containsAll(0, 4, 8) ||
+        Player.playerX.containsAll(2, 4, 6))
+      winner = 'X';
+    else if (Player.playerO.containsAll(0, 1, 2) ||
         Player.playerO.containsAll(3, 4, 5) ||
         Player.playerO.containsAll(6, 7, 8) ||
         Player.playerO.containsAll(0, 3, 6) ||
         Player.playerO.containsAll(1, 4, 7) ||
         Player.playerO.containsAll(2, 5, 8) ||
-        Player.playerO.containsAll(1, 4, 8) ||
+        Player.playerO.containsAll(0, 4, 8) ||
         Player.playerO.containsAll(2, 4, 6))
       winner = 'O';
     else {
@@ -53,19 +55,120 @@ class Game {
   Future autoPlay(activePlayer) async {
     int inx = 0;
     List<int> emptyCells = [];
+
     for (int i = 0; i < 9; i++) {
-      if (!Player.playerX.contains(i) || !Player.playerO.contains(i)) {
+      if (!(Player.playerX.contains(i) || Player.playerO.contains(i))) {
         emptyCells.add(i);
       }
     }
-    Random r = Random();
-    int randomInx = r.nextInt(emptyCells.length);
-    /*while (Player.playerX.contains(randomInx) ||
-        Player.playerO.contains(randomInx)) {
-      randomInx = r.nextInt(emptyCells.length);
+    //start center(Attack)
+    if (Player.playerO.containsAll(0, 1) && emptyCells.contains(2))
+      inx = 2;
+    else if (Player.playerO.containsAll(3, 4) && emptyCells.contains(5))
+      inx = 5;
+    else if (Player.playerO.containsAll(6, 7) && emptyCells.contains(8))
+      inx = 8;
+    else if (Player.playerO.containsAll(0, 3) && emptyCells.contains(6))
+      inx = 6;
+    else if (Player.playerO.containsAll(1, 4) && emptyCells.contains(7))
+      inx = 7;
+    else if (Player.playerO.containsAll(2, 5) && emptyCells.contains(8))
+      inx = 8;
+    else if (Player.playerO.containsAll(0, 4) && emptyCells.contains(8))
+      inx = 8;
+    else if (Player.playerO.containsAll(2, 4) && emptyCells.contains(6))
+      inx = 6;
+    //start end(Attack)
+    else if (Player.playerO.containsAll(0, 2) && emptyCells.contains(1))
+      inx = 1;
+    else if (Player.playerO.containsAll(3, 5) && emptyCells.contains(4))
+      inx = 4;
+    else if (Player.playerO.containsAll(6, 8) && emptyCells.contains(7))
+      inx = 7;
+    else if (Player.playerO.containsAll(0, 6) && emptyCells.contains(3))
+      inx = 3;
+    else if (Player.playerO.containsAll(1, 7) && emptyCells.contains(4))
+      inx = 4;
+    else if (Player.playerO.containsAll(2, 8) && emptyCells.contains(5))
+      inx = 5;
+    else if (Player.playerO.containsAll(0, 8) && emptyCells.contains(4))
+      inx = 4;
+    else if (Player.playerO.containsAll(2, 6) && emptyCells.contains(4))
+      inx = 4;
+    //center end(Attack)
+    else if (Player.playerO.containsAll(2, 1) && emptyCells.contains(0))
+      inx = 0;
+    else if (Player.playerO.containsAll(5, 4) && emptyCells.contains(3))
+      inx = 3;
+    else if (Player.playerO.containsAll(8, 7) && emptyCells.contains(6))
+      inx = 6;
+    else if (Player.playerO.containsAll(6, 3) && emptyCells.contains(0))
+      inx = 0;
+    else if (Player.playerO.containsAll(7, 4) && emptyCells.contains(1))
+      inx = 1;
+    else if (Player.playerO.containsAll(8, 5) && emptyCells.contains(2))
+      inx = 2;
+    else if (Player.playerO.containsAll(8, 4) && emptyCells.contains(0))
+      inx = 0;
+    else if (Player.playerO.containsAll(6, 4) && emptyCells.contains(2))
+      inx = 2;
+    //start center(Defence)
+    else if (Player.playerX.containsAll(0, 1) && emptyCells.contains(2))
+      inx = 2;
+    else if (Player.playerX.containsAll(3, 4) && emptyCells.contains(5))
+      inx = 5;
+    else if (Player.playerX.containsAll(6, 7) && emptyCells.contains(8))
+      inx = 8;
+    else if (Player.playerX.containsAll(0, 3) && emptyCells.contains(6))
+      inx = 6;
+    else if (Player.playerX.containsAll(1, 4) && emptyCells.contains(7))
+      inx = 7;
+    else if (Player.playerX.containsAll(2, 5) && emptyCells.contains(8))
+      inx = 8;
+    else if (Player.playerX.containsAll(0, 4) && emptyCells.contains(8))
+      inx = 8;
+    else if (Player.playerX.containsAll(2, 4) && emptyCells.contains(6))
+      inx = 6;
+    //start end(Defence)
+    else if (Player.playerX.containsAll(0, 2) && emptyCells.contains(1))
+      inx = 1;
+    else if (Player.playerX.containsAll(3, 5) && emptyCells.contains(4))
+      inx = 4;
+    else if (Player.playerX.containsAll(6, 8) && emptyCells.contains(7))
+      inx = 7;
+    else if (Player.playerX.containsAll(0, 6) && emptyCells.contains(3))
+      inx = 3;
+    else if (Player.playerX.containsAll(1, 7) && emptyCells.contains(4))
+      inx = 4;
+    else if (Player.playerX.containsAll(2, 8) && emptyCells.contains(5))
+      inx = 5;
+    else if (Player.playerX.containsAll(0, 8) && emptyCells.contains(4))
+      inx = 4;
+    else if (Player.playerX.containsAll(2, 6) && emptyCells.contains(4))
+      inx = 4;
+    //center end(Defence)
+    else if (Player.playerX.containsAll(2, 1) && emptyCells.contains(0))
+      inx = 0;
+    else if (Player.playerX.containsAll(5, 4) && emptyCells.contains(3))
+      inx = 3;
+    else if (Player.playerX.containsAll(8, 7) && emptyCells.contains(6))
+      inx = 6;
+    else if (Player.playerX.containsAll(6, 3) && emptyCells.contains(0))
+      inx = 0;
+    else if (Player.playerX.containsAll(7, 4) && emptyCells.contains(1))
+      inx = 1;
+    else if (Player.playerX.containsAll(8, 5) && emptyCells.contains(2))
+      inx = 2;
+    else if (Player.playerX.containsAll(8, 4) && emptyCells.contains(0))
+      inx = 0;
+    else if (Player.playerX.containsAll(6, 4) && emptyCells.contains(2))
+      inx = 2;
+    else {
+      Random r = Random();
+      int randomInx = r.nextInt(emptyCells.length);
+
+      inx = emptyCells[randomInx];
     }
-    print(randomInx);*/
-    inx = emptyCells[randomInx];
     playGame(inx, activePlayer);
   }
 }
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -13,7 +13,7 @@ class _HomePageState extends State<HomePage> {
   String activePlayer = 'X';
   bool gameOver = false;
   int turn = 0; //if 9 game over
-  String result = 'Temp';
+  String result = '';
   Game game = Game();
 
   bool isSwitched = false;
@@ -23,99 +23,128 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
       body: SafeArea(
-        child: Column(
-          children: [
-            SwitchListTile.adaptive(
-              title: const Text(
-                'Ture on/off 2 Players',
-                style: TextStyle(color: Colors.white, fontSize: 28),
-                textAlign: TextAlign.center,
-              ),
-              value: isSwitched,
-              onChanged: (bool newValue) {
-                setState(() {
-                  isSwitched = newValue;
-                });
-              },
-            ),
-            Text(
-              'It\'s $activePlayer Turn'.toUpperCase(),
-              style: const TextStyle(color: Colors.white, fontSize: 52),
-              textAlign: TextAlign.center,
-            ),
-            Expanded(
-              child: GridView.count(
-                crossAxisCount: 3,
-                mainAxisSpacing: 8.0,
-                childAspectRatio: 1.0,
-                crossAxisSpacing: 8.0,
-                padding: const EdgeInsets.all(16),
-                children: List.generate(9, (index) {
-                  return InkWell(
-                    onTap: gameOver ? null : () => _onTap(index),
-                    borderRadius: BorderRadius.circular(15),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).shadowColor,
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: Center(
-                        child: Text(
-                          Player.playerX.contains(index)
-                              ? 'X'
-                              : Player.playerO.contains(index)
-                                  ? 'O'
-                                  : '',
-                          style: TextStyle(
-                              color: Player.playerX.contains(index)
-                                  ? Colors.blue
-                                  : Colors.pink,
-                              fontSize: 52),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
+        child: MediaQuery.of(context).orientation == Orientation.portrait
+            ? Column(
+                children: [
+                  ...firstBlock(), //... extract the elements from the list
+                  buildXOGrid(context),
+                  ...lastBlock(),
+                ],
+              )
+            : Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ...firstBlock(),
+                        ...lastBlock(),
+                      ],
                     ),
-                  );
-                }),
+                  ),
+                  buildXOGrid(context),
+                ],
               ),
-            ),
-            Text(
-              result,
-              style: const TextStyle(color: Colors.white, fontSize: 42),
-              textAlign: TextAlign.center,
-            ),
-            ElevatedButton.icon(
-              onPressed: () {
-                setState(() {
-                  activePlayer = 'X';
-                  gameOver = false;
-                  turn = 0; //if 9 game over
-                  result = '';
-                  Player.playerX = [];
-                  Player.playerO = [];
-                });
-              },
-              icon: const Icon(Icons.replay),
-              label: const Text('Repeat The Game'),
-              style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all(Theme.of(context).splashColor),
-              ),
-            ),
-          ],
+      ),
+    );
+  }
+
+  List<Widget> firstBlock() {
+    return [
+      SwitchListTile.adaptive(
+        title: const Text(
+          'Ture on/off 2 Players',
+          style: TextStyle(color: Colors.white, fontSize: 28),
+          textAlign: TextAlign.center,
         ),
+        value: isSwitched,
+        onChanged: (bool newValue) {
+          setState(() {
+            isSwitched = newValue;
+          });
+        },
+      ),
+      Text(
+        'It\'s $activePlayer Turn'.toUpperCase(),
+        style: const TextStyle(color: Colors.white, fontSize: 52),
+        textAlign: TextAlign.center,
+      ),
+    ];
+  }
+
+  List<Widget> lastBlock() {
+    return [
+      Text(
+        result,
+        style: const TextStyle(color: Colors.white, fontSize: 42),
+        textAlign: TextAlign.center,
+      ),
+      ElevatedButton.icon(
+        onPressed: () {
+          setState(() {
+            activePlayer = 'X';
+            gameOver = false;
+            turn = 0; //if 9 game over
+            result = '';
+            Player.playerX = [];
+            Player.playerO = [];
+          });
+        },
+        icon: const Icon(Icons.replay),
+        label: const Text('Repeat The Game'),
+        style: ButtonStyle(
+          backgroundColor:
+              MaterialStateProperty.all(Theme.of(context).splashColor),
+        ),
+      ),
+    ];
+  }
+
+  Expanded buildXOGrid(BuildContext context) {
+    return Expanded(
+      child: GridView.count(
+        crossAxisCount: 3,
+        mainAxisSpacing: 8.0,
+        childAspectRatio: 1.1,
+        crossAxisSpacing: 8.0,
+        padding: const EdgeInsets.all(16),
+        children: List.generate(9, (index) {
+          return InkWell(
+            onTap: gameOver ? null : () => _onTap(index),
+            borderRadius: BorderRadius.circular(15),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).shadowColor,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Center(
+                child: Text(
+                  Player.playerX.contains(index)
+                      ? 'X'
+                      : Player.playerO.contains(index)
+                          ? 'O'
+                          : '',
+                  style: TextStyle(
+                      color: Player.playerX.contains(index)
+                          ? Colors.blue
+                          : Colors.pink,
+                      fontSize: 52),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          );
+        }),
       ),
     );
   }
 
   _onTap(int index) async {
-
     if ((Player.playerX.isEmpty || !Player.playerX.contains(index)) &&
         (Player.playerO.isEmpty || !Player.playerO.contains(index))) {
-
       game.playGame(index, activePlayer);
       updateState();
-      if(!isSwitched && !gameOver){
+      if (!isSwitched && !gameOver && turn != 9) {
         await game.autoPlay(activePlayer);
         updateState();
       }
@@ -125,10 +154,13 @@ class _HomePageState extends State<HomePage> {
   void updateState() {
     setState(() {
       activePlayer = activePlayer == 'X' ? 'O' : 'X';
+      turn++;
+
       String winner = game.checkWinner();
-      if(winner != ''){
+      if (winner != '') {
+        gameOver = true;
         result = '$winner is the winner';
-      }else{
+      } else if (!gameOver && turn == 9) {
         result = 'Draw';
       }
     });
